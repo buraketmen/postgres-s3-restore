2537308a4a51ad51688675f13081229c2414288c
Burak Ketmen <45431865+buraketmen@users.noreply.github.com>
2021-07-04 15:26:23 +0300

    python3

--- install.sh
+++ install.sh
@@ -5,11 +5,8 @@ set -e
 
 apk update
 
-# install pg_dump
-apk add postgresql
-
-# install s3 tools
-apk add python py-pip
+apk add postgresql-client
+apk add python3 py3-pip
 pip install awscli
 apk del py-pip
 
